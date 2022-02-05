c1f95bbf0f1eb815db093330f21098e0fc112ffe
George Garcia <jorge.90gap@gmail.com>
2022-02-05 21:47:15 +0900

    :hammer: React-rails gem settings and webpacks custom

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// this are steps to add from https://github.com/rails/webpacker/tree/5-x-stable
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,47 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery',
-    })
-)
+// Tvoz added  in according to: https://github.com/rails/webpacker/blob/5-x-stable/docs/webpack.md
+const customConfig = require('./custom')
+// const merge = require('webpack-merge')    Needs webpack merge first, called if need it 
+const url = require('./loaders/url')
+
+// loaders
+
+environment.loaders.prepend('url', url)
+
+
+
+// Set nested object prop using path notation
+environment.config.set('resolve.extensions', ['.foo', '.bar'])
+environment.config.set('output.filename', '[name].js')
+
+// Merge custom config
+environment.config.merge(customConfig)
+environment.config.merge({ devtool: 'none' })
+
+// Delete a property
+environment.config.delete('output.chunkFilename')
+
+
+//  For access  to configs within Webpacker's configuration, you can import them like so:
+const { config } = require('@rails/webpacker')
+console.log(config.output_path)
+console.log(config.source_path)
+// 
+
+// Overriding Loader Options in webpack 3+ (for CSS Modules etc.)
+// const myCssLoaderOptions = {
+//   modules: {
+//     localIdentName: '[name]__[local]___[hash:base64:5]'
+//   },
+//   sourceMap: true,
+// }
+
+// const CSSLoader = environment.loaders.get('sass').use.find(el => el.loader === 'css-loader')
+
+// CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions)
+// 
+
+
 
 module.exports = environment
