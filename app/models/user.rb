0e3d134f579e021a81024a5c73d93ea8759801cb
George Garcia <jorge.90gap@gmail.com>
2022-02-07 13:28:02 +0900

    :hammer_wrench User(Admin) created by Devise, migrated to schema and settings for access

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, uniqueness: true
+  validates :email, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
+  validates :password, length: { minimum: 6 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
